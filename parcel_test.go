6630ef744bfc8f5165f845439d572d1280e03b39
Ig@reK <kigorek@mail.ru>
2026-02-18 00:58:41 +0300

    modified:   .github/workflows/build.yml

--- parcel_test.go
+++ parcel_test.go
@@ -52,7 +52,7 @@ func TestAddGetDelete(t *testing.T) {
 	require.Equal(t, parcel, stored)
 
 	// delete
-	err = store.Delete(parcel.Number)
+	_ = store.Delete(parcel.Number)
 
 	_, err = store.Get(parcel.Number)
 	require.Equal(t, sql.ErrNoRows, err)
